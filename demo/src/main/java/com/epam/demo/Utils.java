e512162ab4435dc00a71036b70a691da089302ba
узаффар лдашев <myuldashev@paymart.uz>
2023-04-11 16:19:40 +0500

    1 attempt

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,12 @@
 package com.epam.demo;
 
+import com.sun.tools.javac.util.StringUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
        //magic happens here
+        return args.stream().allMatch(StringUtils::isPositiveNumber);
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,9 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        return NumberUtils.isCreatable(str) && NumberUtils.toDouble(str) > 0;
     }
 }
